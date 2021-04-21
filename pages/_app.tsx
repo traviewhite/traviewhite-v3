d9814a55d4037e620130201ea38c0ae817f6a743
Travis White <traviewhite@gmail.com>
2021-04-20 19:32:09 -0500

    Implemented nprogress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,29 @@
 import 'styles/globals.css'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
 import { AnimatePresence } from 'framer-motion'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 import Layout from 'components/Layout'
+import Meta from 'components/Meta'
 
 function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  useEffect(() => {
+    router.events.on('routeChangeStart', () => NProgress.start())
+    router.events.on('routeChangeComplete', () => NProgress.done())
+    router.events.on('routeChangeError', () => NProgress.done())
+  }, [])
+
   return (
     <ThemeProvider attribute='class' defaultTheme={'system' ? 'dark' : 'system'} disableTransitionOnChange>
+      <Meta />
       <Layout>
         <AnimatePresence exitBeforeEnter>
-          <Component {...pageProps} />
+          <Component {...pageProps} key={router.route} />
         </AnimatePresence>
       </Layout>
     </ThemeProvider>
