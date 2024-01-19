d0cc2783f90fb5fec90aaee9eba17b4ac934679d
Travis White <traviewhite@gmail.com>
2024-01-19 10:56:50 -0600

    temp: remove extra links from nav

--- components/Navigation/Links.tsx
+++ components/Navigation/Links.tsx
@@ -27,35 +27,35 @@ const links = [
       </svg>
     ),
   },
-  {
-    label: 'Blog',
-    href: '/blog',
-    icon: (
-      <svg xmlns='http://www.w3.org/2000/svg' className='h-5 w-5' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
-        <path
-          strokeLinecap='round'
-          strokeLinejoin='round'
-          strokeWidth={1.75}
-          d='M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z'
-        />
-      </svg>
-    ),
-  },
-  {
-    label: 'Photo',
-    href: '/photo',
-    icon: (
-      <svg xmlns='http://www.w3.org/2000/svg' className='h-5 w-5' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
-        <path
-          strokeLinecap='round'
-          strokeLinejoin='round'
-          strokeWidth={1.75}
-          d='M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z'
-        />
-        <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M15 13a3 3 0 11-6 0 3 3 0 016 0z' />
-      </svg>
-    ),
-  },
+  // {
+  //   label: 'Blog',
+  //   href: '/blog',
+  //   icon: (
+  //     <svg xmlns='http://www.w3.org/2000/svg' className='h-5 w-5' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
+  //       <path
+  //         strokeLinecap='round'
+  //         strokeLinejoin='round'
+  //         strokeWidth={1.75}
+  //         d='M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z'
+  //       />
+  //     </svg>
+  //   ),
+  // },
+  // {
+  //   label: 'Photo',
+  //   href: '/photo',
+  //   icon: (
+  //     <svg xmlns='http://www.w3.org/2000/svg' className='h-5 w-5' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
+  //       <path
+  //         strokeLinecap='round'
+  //         strokeLinejoin='round'
+  //         strokeWidth={1.75}
+  //         d='M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z'
+  //       />
+  //       <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M15 13a3 3 0 11-6 0 3 3 0 016 0z' />
+  //     </svg>
+  //   ),
+  // },
   {
     label: 'About',
     href: '/about',
