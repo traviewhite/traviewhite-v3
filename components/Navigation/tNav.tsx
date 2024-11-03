f9c97f9b66ba78f170d5e14e33269c060779c8a7
Travis White <traviewhite@gmail.com>
2024-11-02 21:33:42 -0500

    refactor: simplify tnav comp

--- components/Navigation/tNav.tsx
+++ components/Navigation/tNav.tsx
@@ -1,58 +1,35 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
-import { motion } from 'framer-motion'
+import { motion, AnimatePresence } from 'framer-motion'
 
 const tLogo: string = '/t_logo.svg'
 const tWhiteLogo: string = '/twhite_logo.svg'
 
 const Tnav: React.FC = () => {
   const router = useRouter()
+  const isHome = router.route === '/'
 
-  const isHome = router.route === `/`
-
-  if (isHome) {
-    return (
+  return (
+    <AnimatePresence mode='wait'>
       <motion.div
+        key={isHome ? 'home' : 'other'}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.1 }}
         whileHover={{
           x: 1,
           y: 2,
           transition: { duration: 0.1 },
         }}
-        className='py-7 px-16 mr-auto relative'
+        className={`py-7 ${isHome ? 'px-16' : 'px-6'} mr-auto relative`}
       >
         <Link href='/'>
-          <Image
-            fill
-            src={tWhiteLogo}
-            alt='traviewhite T logo'
-            // height={60}
-            // width={130}
-          />
+          <Image src={isHome ? tWhiteLogo : tLogo} alt='traviewhite T logo' layout='fill' />
         </Link>
       </motion.div>
-    )
-  }
-
-  return (
-    <motion.div
-      whileHover={{
-        x: 1,
-        y: 2,
-        transition: { duration: 0.1 },
-      }}
-      className='py-7 px-6 mr-auto relative'
-    >
-      <Link href='/'>
-        <Image
-          fill
-          src={tLogo}
-          alt='traviewhite T logo'
-          // height={60}
-          // width={40}
-        />
-      </Link>
-    </motion.div>
+    </AnimatePresence>
   )
 }
 
